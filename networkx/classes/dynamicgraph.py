2dd54e1c0bc3eb5325a80262f33d6a3dc5bb3cb7
jama <jamakazie@gmail.com>
2017-06-14 00:05:13 -0400

    Added this into a new branch "architecture".  The goal of this is to define the interface (classes, method and their signatures) for this project.

---
+++ networkx/classes/dynamicgraph.py
@@ -0,0 +1,56 @@
+""" Base class for dynamic graphs.
+
+"""
+import networkx as nx
+
+class DynamicGraph(object):
+
+    def __init__(self, edgelist, **attr):
+        """ Initialize a continuous dynamic graph with edges, name, and graph attributes.
+
+        Parameters
+        ----------
+        edgelist : 4-tuple (u,v,start_time, end_time) indicating an edge
+            between node u, node v, begining at start_time, and finishing at 
+            end_time
+        name : string, optional (default='')
+            An optional name for the graph.
+        attr : keyword arguments, optional (default= no attributes)
+            Attributes to add to graph as key=value pairs.
+        """
+        pass
+
+    def __str__(self):
+        pass
+
+    def __contains__(self, n):
+        pass
+
+    def add_node(self, n, **attr):
+        """ Adds node n to the Dynamic Graph
+        """
+        pass
+
+    def add_edge(self, u, v, start_time, end_time):
+        """ Creates an undirected edge between node u and node v,
+            begining at start_time and finishing at end_time
+        """
+        pass
+
+    def timestamp_filter(self, start_time, end_time):
+        """ Creates a static graph of all nodes and edges that exist between
+            start_time and end_time
+        """
+        pass
+
+    def node_filter(self, nbunch):
+        """ Creates a dynamic subgraph consisting of only nodges and edges that
+            are in nbunch
+        """
+        pass
+
+    def coarsen(self):
+        """ Returns a dynamic graph where multiple nodes have been combined 
+            together into a supernode
+        """
+        pass
