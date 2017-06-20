53b543e06f034c87bb4eb3345bbea3cab100bf36
jama <jamakazie@gmail.com>
2017-06-19 20:24:58 -0400

    Added (failing) tests for the dynamic graph, these tests can be ran with the command:     nosetests test_dynamicgraph.py

---
+++ networkx/classes/tests/test_dynamicgraph.py
@@ -0,0 +1,32 @@
+from nose.tools import assert_equal
+from nose.tools import assert_is
+from nose.tools import assert_not_equal
+from nose.tools import assert_raises
+from nose.tools import assert_true
+from nose.tools import raises
+
+import networkx
+
+class TestDynamicGraph(object):
+    """ Tests dynamic graph class"""
+
+    def test_contains(self):
+        assert(False)
+
+    def test_add_node(self):
+        assert(False)
+
+    def test_add_edge(self):
+        assert(False)
+
+    def test_timestamp_filter(self):
+        assert(False)
+
+    def test_node_filter(self):
+        assert(False)
+
+    def test_coarsen(self):
+        assert(False)
+
+    def test_to_snapshots(self):
+        assert(False)
