0aac0d6b219cd7b4aad9978d7bd1380e50ce3d49
Makan Arastuie <makan.arastuie@gmail.com>
2018-03-06 15:11:49 -0500

    Create test graph

--- networkx/classes/tests/test_dynamicgraph.py
+++ networkx/classes/tests/test_dynamicgraph.py
@@ -9,6 +9,21 @@ import networkx
 
 class TestDynamicGraph(object):
     """ Tests dynamic graph class"""
+    def setup(self):
+        self.graph = networkx.dynamicgraph()
+        ed1, ed2, ed3, ed4, ed5, ed6, ed7 = ({}, {}, {}, {}, {}, {}, {})
+        self.k7adj = {0: {1: ed1, 2: ed2},
+                      1: {0: ed1, 2: ed3},
+                      2: {0: ed2, 1: ed3}}
+        self.k7edges = [(0, 1), (0, 2), (1, 2)]
+        self.k7nodes = [0, 1, 2, 3, 4, 5, 6]
+        self.K7 = self.Graph()
+        self.K7.adj = self.K7.edge = self.k3adj
+        self.K7.node = {}
+        self.K7.node[0] = {}
+        self.K7.node[1] = {}
+        self.K7.node[2] = {}
+        assert(False)
 
     def test_contains(self):
         assert(False)
