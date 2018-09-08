9fa88f7234c674b65a4f99cb5d7e349c4bedc38e
msloma144 <syphr144@gmail.com>
2018-09-08 17:54:10 -0400

    Added more comments, added get method

--- dynetworkx/classes/snapshotgraph.py
+++ dynetworkx/classes/snapshotgraph.py
@@ -1,6 +1,4 @@
 from networkx.classes.graph import Graph
-import networkx as nx
-import networkx.algorithms.isomorphism as iso
 
 class SnapshotGraph(object):
     def __init__(self, **attr):
@@ -14,6 +12,7 @@ class SnapshotGraph(object):
 
     def generator(self, start, end):
         """
+        Generator used for producing lists of snapshots in a memory efficient way.
 
         Parameters
         ----------
@@ -49,15 +48,44 @@ class SnapshotGraph(object):
                 snaps += 1
 
     def check_equality(self, g1, g2):
+        """
+        Custom equality check between two graphs based on attributes,
+        edge objects and node objects.
+
+        Parameters
+        ----------
+        g1: networkx graph object
+        g2: networkx graph object
+
+        Returns
+        -------
+
+        """
         return (g1._adj == g2._adj) and (g1.edges == g2.edges) and (g1.nodes == g2.nodes)
 
     def insert(self, g_to_insert, snap_len=None, num_in_seq=None):
+        """
+        Insert a graph into the snapshotgraph at a given index, with some snapshot length.
+
+        Parameters
+        ----------
+        g_to_insert: networkx graph object
+            networkx graph to be inserted into snapshot graph.
+        snap_len: Integer
+            Length of the snapshot.
+        num_in_seq: Integer
+            Time slot to begin insertion at.
+
+
+        Returns
+        -------
+        None
+
+        """
         snaps_traveled = 0
         snaps = 0
 
         while snaps < len(self.snapshots):
-            iters = 0
-
             # if next snap length + total traveled thus far is less than slot needed,
             # then just keep moving
             if snaps_traveled + self.snapshots[snaps][1] < num_in_seq:
@@ -84,6 +112,7 @@ class SnapshotGraph(object):
 
     def add_snapshot(self, ebunch, num_in_seq=None, weight='weight', **attr):
         """
+        Add a snapshot with a bunch of edge values.
 
         Parameters
         ----------
@@ -91,6 +120,7 @@ class SnapshotGraph(object):
 
         Returns
         -------
+        None
 
         """
         # add snapshot assumes appendng to end, shoudl I combline with insert method? probs, but also need insert method
@@ -193,6 +223,7 @@ class SnapshotGraph(object):
 
     def number_of_nodes(self, sbunch=None):
         """
+        Gets number of nodes in each snapshot requested in 'sbunch'.
 
         Parameters
         ----------
@@ -217,6 +248,7 @@ class SnapshotGraph(object):
 
     def order(self, sbunch=None):
         """
+        Gets order of each node requested in 'sbunch'.
 
         Parameters
         ----------
@@ -241,6 +273,7 @@ class SnapshotGraph(object):
 
     def has_node(self, n, sbunch=None):
         """
+        Gets boolean list of if a snapshot in 'sbunch' contains node 'n'.
 
         Parameters
         ----------
@@ -375,3 +408,23 @@ class SnapshotGraph(object):
         graph_list = [graph_list[index - min_index] for index in sbunch]
 
         return [g.size(weight=weight) for g in graph_list]
+
+    def get(self, sbunch=None):
+        """
+        Gets all graphs in snapshot graph requested in sbunch.
+        
+        Parameters
+        ----------
+        sbunch
+
+        Returns
+        -------
+
+        """
+        min_index = min(sbunch)
+        max_index = max(sbunch)
+        # get all indexes between min and max
+        graph_list = list(self.generator(min_index, max_index))
+        # only get the indexes wanted
+        graph_list = [graph_list[index - min_index] for index in sbunch]
+        return graph_list
